99d8a3e781d1df841b8ed0645f52fc8afb692f24
NUTT CHOKWITTAYA <15667217+ACHEER1420@users.noreply.github.com>
2020-11-21 15:09:03 -0600

    [TDD-fizzbuzz-4] - implement fizzbuzz funtion to return FizzBuzz when the number is divisible by both 3 and 5

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,10 +1,11 @@
 function fizzbuzz(number) {
   if (number < 0) return;
-  if (number % 3 === 0) {
-    return 'Fizz';
-  }
 
-  if(number % 5 === 0) {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return 'FizzBuzz';
+  } else if (number % 3 === 0) {
+    return 'Fizz';
+  } else if (number % 5 === 0) {
     return 'Buzz';
   }
   return 1;
