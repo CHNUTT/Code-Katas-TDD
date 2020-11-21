ac7742167af409d47de2a518601718a3df73a1ee
NUTT CHOKWITTAYA <15667217+ACHEER1420@users.noreply.github.com>
2020-11-21 14:57:38 -0600

    [TDD-Test-2] - implement test for numbers that only divisible by 3 and return fizz

--- __test__/fizzbuzz.test.js
+++ __test__/fizzbuzz.test.js
@@ -2,10 +2,10 @@ const fizzbuzz = require('../src/fizzbuzz');
 /**
  * Test
  * 1. only positive number
- * 2. if not devided by 3 or 5 print that number
- * 3. if devided by 3 only print Fizz
- * 4. if devided by 5 only print Buzz
- * 5. if devided by both 3 and 5 print FizzBuzz
+ * 2. if devided by 3 only print Fizz
+ * 3. if devided by 5 only print Buzz
+ * 4. if devided by both 3 and 5 print FizzBuzz
+ * 5. if not devided by 3 or 5 print that number
  */
 describe('fizzbuzz function', () => {
   it('should only receive a positive number as an input', () => {
@@ -13,5 +13,12 @@ describe('fizzbuzz function', () => {
     expect(fizzbuzz(-10)).toBeFalsy();
     expect(fizzbuzz(1)).toBeTruthy();
     expect(fizzbuzz(0)).toBeTruthy();
+  });
+
+  it('should print Fizz if the input number is only divisible by 3', () => {
+    expect(fizzbuzz(3)).toEqual('Fizz');
+    expect(fizzbuzz(6)).toEqual('Fizz');
+    expect(fizzbuzz(2)).not.toEqual('Fizz');
+    expect(fizzbuzz(5)).not.toEqual('Fizz');
   });
 });
